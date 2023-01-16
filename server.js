43ba4885395413a7262274064103cc40990817d8
Florent TABOUKOUNA <56768827+Floturner@users.noreply.github.com>
2023-01-16 22:50:44 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+	while (true) {
+		console.log("Microservices rock!");
+		await sleep(5000);
+	}
+}
+
+main();
