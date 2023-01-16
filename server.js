27fdf7f56684d0d342c35b31802b472e8d22575b
Florent TABOUKOUNA <56768827+Floturner@users.noreply.github.com>
2023-01-16 23:05:09 +0000

    added env vars

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const FAVORITE_GAME = process.env.FAVORITE_GAME;
+
 function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
 	while (true) {
-		console.log("Microservices rock!");
+		console.log(`I love ${FAVORITE_GAME}`);
 		await sleep(5000);
 	}
 }
